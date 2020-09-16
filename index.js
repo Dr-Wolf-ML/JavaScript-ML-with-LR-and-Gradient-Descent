94bb6b8320ef0be67807a106c3a46ac0d3944cef
Dr-Wolf-ML <wolfgangml@mac.com>
2020-09-16 10:57:20 +1000

    refactor to Tensorflow complete, incl node-remote-plot

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const tf = require('@tensorflow/tfjs');
+const plot = require('node-remote-plot');
 
 const LinearRegression = require('./linear-regression');
-
 const loadCSV = require('./load-csv');
 
 let {features, labels, testFeatures, testLabels} = loadCSV('./cars.csv', {
-  dataColumns: ['horsepower'],
+  dataColumns: ['horsepower', 'weight', 'displacement'],
   labelColumns: ['mpg'],
   converters: {},
   shuffle: true,
@@ -13,10 +13,23 @@ let {features, labels, testFeatures, testLabels} = loadCSV('./cars.csv', {
 });
 
 const regression = new LinearRegression(features, labels, {
-  learningRate: 0.0001,
+  learningRate: 0.1,
   iterations: 100
 });
 
 regression.train();
 
-console.log('Updated m: ', regression.m, '  Updated b: ', regression.b);
+// Select either one plot at a time
+
+plot({
+  x: regression.mseHistory.reverse(),
+  xLabel: 'Iterations #',
+  yLabel: 'MSE - Mean Squared Error'
+})
+
+// plot({
+//   x: regression.bHistory,
+//   y: regression.mseHistory.reverse(),
+//   xLabel: 'value of B',
+//   yLabel: 'MSE - Mean Squared Error'
+// });
--- linear-regression.js
+++ linear-regression.js
@@ -1,41 +1,109 @@
 const tf = require('@tensorflow/tfjs');
-const _ = require('lodash');
 
 class LinearRegression {
   constructor(features, labels, options) {
     // expected to be passed as tensorflow Tensors:
-    this.features = features;
-    this.labels = labels;
+    this.features = this.processFeatures(features);
+    this.labels = tf.tensor(labels);
+    this.mseHistory = [];
+    this.bHistory = [];
 
     this.options = Object.assign({
       learningRate: 0.1,
       iterations: 1000
     }, options)
 
-    this.m = 0;
-    this.b = 0;
+    this.weights = tf.zeros([this.features.shape[1],1]);
   }
 
   gradientDescent() {
-    const currentGuessesforMPG = this.features.map(row => {
-      return this.m * row[0] + this.b;
-    });
+    const currentGuesses = this.features.matMul(this.weights);
+    const differences = currentGuesses.sub(this.labels);
+    const slopes = this.features  // slopes is sometimes called 'gradients'
+      .transpose()
+      .matMul(differences)
+      .div(this.features.shape[0])
+      // .mul(2) is require in the original equation, but doesn't add any real value here !!
 
-    const bSlope = _.sum(currentGuessesforMPG.map((guess, i) => {
-      return guess - this.labels[i][0];
-    })) * 2 / this.features.length;
-
-    const mSlope = _.sum(currentGuessesforMPG.map((guess, i) => {
-      return -1 * this.features[i][0] * (this.labels[i][0] - guess);
-    })) * 2 / this.features.length;
-
-    this.m -= mSlope * this.options.learningRate;
-    this.b -= bSlope * this.options.learningRate;
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
     for (let i = 0; i < this.options.iterations; i++) {
+      this.bHistory.push(this.weights.arraySync()[0][0]);
       this.gradientDescent();
+      this.recordMSE();
+      this.updateLearningRate();
+    }
+  }
+
+  test(testFeatures, testLabels) {
+    testFeatures = this.processFeatures(testFeatures);
+    testLabels = tf.tensor(testLabels);
+
+    const predictions = testFeatures.matMul(this.weights);
+
+    // Sum of Squares Residual
+    const SSres = testLabels.sub(predictions)
+      .pow(2)
+      .sum()
+      .arraySync()
+
+    // Sum of Squares Total
+    const SStot = testLabels
+      .sub(testLabels.mean())
+      .pow(2)
+      .sum()
+      .arraySync()
+
+    // R^2 (= Coefficient of Determination)
+    return 1 - SSres / SStot;
+  }
+
+  processFeatures(features) {
+    features  = tf.tensor(features);
+
+    if (this.mean && this.variance) {
+      features = features.sub(this.mean).div(this.variance.pow(0.5));
+    } else {
+      features = this.standardize(features);
+    }
+
+    features = tf.ones([features.shape[0], 1]).concat(features, 1);
+
+    return features;
+  }
+
+  standardize(features) {
+    const {mean, variance} = tf.moments(features, 0);
+
+    this.mean = mean;
+    this.variance = variance;
+
+    return features.sub(mean).div(variance.pow(0.5));
+  }
+
+  recordMSE() {
+    const mse = this.features
+      .matMul(this.weights)
+      .sub(this.labels)
+      .pow(2)
+      .sum()
+      .div(this.features.shape[0])
+      .arraySync()
+
+    this.mseHistory.unshift(mse);
+  }
+
+  updateLearningRate() {
+    if (this.mseHistory.length < 2) {
+      return;
+    } 
+    
+    if (this.mseHistory[0] > this.mseHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
     }
   }
 }
